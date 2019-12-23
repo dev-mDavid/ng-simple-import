59fbb93b2cf399488a32c280f86679e4faf6d4ae
mDavid <dev.mdavid@gmail.com>
2019-12-23 12:52:40 -0800

    generate Components: A & B and Class C

---
+++ src/app/a/a.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-a',
+  templateUrl: './a.component.html',
+  styleUrls: ['./a.component.sass']
+})
+export class AComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/c.spec.ts
@@ -0,0 +1,7 @@
+import { C } from './c';
+
+describe('C', () => {
+  it('should create an instance', () => {
+    expect(new C()).toBeTruthy();
+  });
+});
