f7c6cefdf785c954a29cdb891303a2b631b3860f
mDavid <dev.mdavid@gmail.com>
2019-12-23 13:21:32 -0800

    inject BComponent into AComponent

--- src/app/a/a.component.ts
+++ src/app/a/a.component.ts
@@ -1,14 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { BComponent } from '../b/b.component';
+
 
 @Component({
   selector: 'app-a',
   templateUrl: './a.component.html',
   styleUrls: ['./a.component.sass']
 })
+
 export class AComponent implements OnInit {
+  isWorking: BComponent;
 
-  constructor() { }
-
+  constructor() {
+    console.log("A works");
+    this.isWorking = new BComponent();
+  }
+  
+  allComponents() {
+    return this.isWorking.bWorks();
+  }
+  // allComponents()
+  
   ngOnInit() {
   }
 
--- src/app/b/b.component.ts
+++ src/app/b/b.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-b',
   templateUrl: './b.component.html',
   styleUrls: ['./b.component.sass']
 })
-export class BComponent implements OnInit {
+export class BComponent{
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
+  bWorks() {
+    console.log("B works");
+}
 
+    
 }
